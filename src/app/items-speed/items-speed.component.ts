f5a854c355d0c8035e078e9c6e96157735924a3b
Pavel Strakhov <ri@idzaaus.org>
2018-03-12 22:32:26 +0300

    table restyle

--- src/app/items-speed/items-speed.component.ts
+++ src/app/items-speed/items-speed.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ItemsSpeedComponent implements OnInit {
   @Input() value: any;
+  @Input() fractionMode: boolean = false;
 
   constructor() { }
 
