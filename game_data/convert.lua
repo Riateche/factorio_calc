f2ca92ccd3e52661c3788dfa1a98787416c3c066
Pavel Strakhov <ri@idzaaus.org>
2018-03-12 22:41:23 +0300

    delete everything

--- game_data/convert.lua
+++
@@ -1,6 +0,0 @@
-if not arg[1] then
-  error("Usage: lua5.3 convert.lua recipes_input.lua > recipes_output.json")
-end
-local JSON = (loadfile "json.lua")()
-local v = loadfile(arg[1])()
-print(JSON:encode_pretty(v))
